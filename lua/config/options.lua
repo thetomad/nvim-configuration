63bff9686afa9347aec00c66a3f8756a534c4167
Toma Dragomir <tomadragomir1@gmail.com>
2026-01-12 13:54:32 +0200

    Added terminal mapping.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,5 +3,11 @@ vim.opt.cursorline = true
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
 
+-- when open terminal buffer, start terminal
 
+vim.api.nvim_create_autocmd("TermOpen", {
+    callback = function()
+	vim.cmd "startinsert!"
+    end,
+})
 
--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -2,3 +2,5 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
+vim.api.nvim_set_keymap("t", "jj", "<C-\\><C-n><C-o>", { noremap=false })
+
