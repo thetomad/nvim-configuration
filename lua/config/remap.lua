e0c6320a7825e3962887ebe3a2bd398488f92def
Toma Dragomir <tomadragomir1@gmail.com>
2026-01-12 14:07:48 +0200

    Added closing paranthesis for ([{.

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -2,5 +2,11 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
+
+-- terminal exit
 vim.api.nvim_set_keymap("t", "jj", "<C-\\><C-n><C-o>", { noremap=false })
 
+-- paranthesis
+vim.api.nvim_set_keymap("i", "{", "{}<Left>", { noremap=false })
+vim.api.nvim_set_keymap("i", "[", "[]<Left>", { noremap=false })
+vim.api.nvim_set_keymap("i", "(", "()<Left>", { noremap=false })
